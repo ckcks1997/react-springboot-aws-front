9fbf40a52293d2f898d445338525e5e5747d2dbe
ckcks1997 <ckcks1997@gmail.com>
2023-03-05 21:25:11 +0900

    ~196p 정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todo from "./Todo";
+import React, { useState } from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from "./AddTodo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "hello world1",
+      done: true,
+    },
+    {
+      id: "1",
+      title: "hello world2",
+      done: true,
+    },
+  ]);
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))}
+      </List>
+    </Paper>
   );
+
+  return <div className="App">
+	<Container maxWidth="md">
+		<AddTodo/>
+		<div className="TodoList">{todoItems}</div>
+	</Container>
+	</div>;
 }
 
 export default App;
